b469b5c4b331f8a70f6eafa2d95a52693a97c47d
Luke <lukedemarinodev@gmail.com>
2022-09-07 16:32:19 -0400

    Abstraction

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 7; 
+    public int forceConst = 50;
     private float horizontalInput;
     private float verticalInput;
+    public bool collision;
+    private Rigidbody selfRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        selfRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        PlayerMovement();
+        Jump();
+    }
+
+    void PlayerMovement()
     {
         // Player Input
         horizontalInput = Input.GetAxis("Horizontal");
@@ -24,6 +33,20 @@ public class PlayerController : MonoBehaviour
         // Player Movement
         transform.Translate(Vector3.right * Time.deltaTime * speed * horizontalInput);
         transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+    }
+
+    void Jump()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) && collision == true)
+        {
+            selfRigidbody.AddForce(0, forceConst, 0, ForceMode.Impulse);
+            collision = false;
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        collision = true;
     }
 }
 
